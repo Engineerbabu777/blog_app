5f18bd6e089a609c461b2338805401f98ce85b2a
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-09 21:29:27 +0500

    add(many-things): leanred about theme a bit , creating statis var and method in it too, created re-useable theme for borders, created reuseable auh fields, added padding and sized box

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,7 +2,22 @@ import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+    borderSide: BorderSide(color: color, width: 3),
+    borderRadius: BorderRadius.circular(10),
+  );
+
   static final darkThemeMode = ThemeData.dark().copyWith(
+    // BACK GROUND COLOR!
     scaffoldBackgroundColor: AppPallete.backgroundColor,
+
+    // INPUT DECORATION THEME!
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient1),
+    ),
+
+    //
   );
 }
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -11,14 +12,30 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Sign Up.",
-            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign Up.",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+        
+            // MARGIN TOP / BLANK SPACE!
+            const SizedBox(height: 30),
+        
+            // INPUTS!
+            AuthField(hintText: "Name"),
+            const SizedBox(height: 10),
+        
+            AuthField(hintText: "Email"),
+            const SizedBox(height: 10),
+        
+            AuthField(hintText: "Password"),
+            const SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
