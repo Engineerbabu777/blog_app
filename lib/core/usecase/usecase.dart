356f5b56e289317737ae1ffa418832442eaf7944
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-11 22:07:52 +0500

    add(UseCase): this has been added

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:blog_app/core/error/failure.dart';
 import 'package:fpdart/fpdart.dart';
 
-abstract interface class UseCase<SucessType> {
-  Future<Either<Failure, SucessType>> call();
+abstract interface class UseCase<SucessType, Params> {
+  Future<Either<Failure, SucessType>> call(Params params);
 }
