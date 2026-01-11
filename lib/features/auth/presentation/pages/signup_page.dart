3e09139bac33526a3765dca374a0fa1448604526
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-11 18:33:42 +0500

    add(file): a new empty file has been added

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -13,7 +13,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -21,17 +21,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
               "Sign Up.",
               style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
             ),
-        
+
             // MARGIN TOP / BLANK SPACE!
             const SizedBox(height: 30),
-        
+
             // INPUTS!
             AuthField(hintText: "Name"),
             const SizedBox(height: 10),
-        
+
             AuthField(hintText: "Email"),
             const SizedBox(height: 10),
-        
+
             AuthField(hintText: "Password"),
             const SizedBox(height: 10),
           ],
