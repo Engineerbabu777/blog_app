8688dd58dc6387c2c81a2fdf04702fa5d41098b4
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-11 21:19:59 +0500

    add(file): a new empty file has been added

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 abstract interface class AuthRemoteDataSource {
   Future<String> signUpWithEmailPassword({
     required String email,
@@ -9,4 +11,30 @@ abstract interface class AuthRemoteDataSource {
     required String email,
     required String password,
   });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {'name': name},
+      );
+
+      if (response.user == null) {
+        // throw Exception('User not found');
+      }
+
+      return response.user!.id;
+    } catch (e) {}
+  }
 }
