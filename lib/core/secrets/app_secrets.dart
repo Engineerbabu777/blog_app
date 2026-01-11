c3269f83bec403bfc0980595b92b7c218110eac4
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-11 20:47:08 +0500

    add(AppSecrets): this has been added

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,3 +1,5 @@
-
-
-class AppSecrets {}
+class AppSecrets {
+  static const supabaseUrl = "https://wnumulomcowkjquqgyou.supabase.co";
+  static const supabaseAnonKey =
+      "sb_publishable_zjuHrDcvPtXYZTycz94EAA_UazkiRBO";
+}
