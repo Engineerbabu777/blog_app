8ef9c32bdd2f45420e69a015b678de88ccf113bc
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 11:54:45 +0500

    add(UserSignIn): this has been implemented

--- lib/features/auth/domain/use-cases/user_sign_in.dart
+++ lib/features/auth/domain/use-cases/user_sign_in.dart
@@ -1,7 +1,21 @@
+import 'package:blog_app/core/error/failure.dart';
 import 'package:blog_app/core/usecase/usecase.dart';
 import 'package:blog_app/features/auth/domain/entities/user_entity.dart';
+import 'package:blog_app/features/auth/domain/respository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
-class UserSignIn implements UseCase<UserEntity, UserSignInParams> {}
+class UserSignIn implements UseCase<UserEntity, UserSignInParams> {
+  final AuthRepository authRepository;
+  UserSignIn({required this.authRepository});
+
+  @override
+  Future<Either<Failure, UserEntity>> call(UserSignInParams params) async {
+    return await authRepository.signInWithEmailPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
 
 class UserSignInParams {
   final String email;
