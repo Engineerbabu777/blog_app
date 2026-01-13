606e48928a263fbae23ba3a6562d65ebec053430
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-13 12:02:49 +0500

    add(AuthBloc): this is now ready for auth sign in

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/features/auth/domain/entities/user_entity.dart';
+import 'package:blog_app/features/auth/domain/use-cases/user_sign_in.dart';
 import 'package:blog_app/features/auth/domain/use-cases/user_sign_up.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,9 +9,11 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
+  final UserSignIn _userSignIn;
 
-  AuthBloc({required UserSignUp userSignUp})
+  AuthBloc({required UserSignUp userSignUp, required UserSignIn userSignIn})
     : _userSignUp = userSignUp,
+      _userSignIn = userSignIn,
       super(AuthInitial()) {
     // EVENT!
     on<AuthSignUp>((event, emit) async {
@@ -32,5 +35,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     });
 
     // EVENT!
+    on<AuthSignIn>((event, emit) async {
+      emit(AuthLoading());
+
+      final res = await _userSignIn(
+        UserSignInParams(email: event.email, password: event.password),
+      );
+
+      res.fold(
+        (l) => emit(
+          AuthFailure(message: l.message ?? "An unknown error occurred"),
+        ),
+        (r) => emit(AuthSuccess(r)),
+      );
+    });
   }
 }
--- lib/init_dep.dart
+++ lib/init_dep.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/env-secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/respository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/use-cases/user_sign_in.dart';
 import 'package:blog_app/features/auth/domain/use-cases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -40,7 +41,11 @@ void _initAuth() {
     () => UserSignUp(authRepository: serviceLocator()),
   );
 
+  serviceLocator.registerFactory<UserSignIn>(
+    () => UserSignIn(authRepository: serviceLocator()),
+  );
+
   serviceLocator.registerLazySingleton(
-    () => AuthBloc(userSignUp: serviceLocator()),
+    () => AuthBloc(userSignUp: serviceLocator(), userSignIn: serviceLocator()),
   );
 }
