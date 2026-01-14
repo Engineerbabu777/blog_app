6a77c079d84c6fa803905d92d2825b2a7b4a9b2a
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-14 13:27:14 +0500

    add(CurrentUser): this is dine

--- lib/features/auth/domain/use-cases/current_user.dart
+++ lib/features/auth/domain/use-cases/current_user.dart
@@ -1,4 +1,18 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/entities/user_entity.dart';
+import 'package:blog_app/features/auth/domain/respository/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
 
+class CurrentUser implements UseCase<UserEntity?, NoParams> {
+  final AuthRepository authRepository;
 
+  CurrentUser({required this.authRepository});
 
-class CurrentUser {}
+  @override
+  Future<Either<Failure, UserEntity?>> call(NoParams params) async {
+    return await authRepository.getCurrentUser();
+  }
+}
+
+class NoParams {}
