5c8af1a61f93e893a6e4f04ea50804c4f49f7e06
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-14 13:43:08 +0500

    add(_initAuth): this has been added

--- lib/init_dep.dart
+++ lib/init_dep.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/env-secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/respository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/use-cases/current_user.dart';
 import 'package:blog_app/features/auth/domain/use-cases/user_sign_in.dart';
 import 'package:blog_app/features/auth/domain/use-cases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
@@ -37,8 +38,14 @@ void _initAuth() {
     ..registerFactory<UserSignIn>(
       () => UserSignIn(authRepository: serviceLocator()),
     )
+    ..registerFactory<CurrentUser>(
+      () => CurrentUser(authRepository: serviceLocator()),
+    )
     ..registerLazySingleton(
-      () =>
-          AuthBloc(userSignUp: serviceLocator(), userSignIn: serviceLocator()),
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        userSignIn: serviceLocator(),
+        currentUser: serviceLocator(),
+      ),
     );
 }
