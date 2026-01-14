2fdae04b486a4b7996635c82816e8936250b0cd2
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-14 13:45:02 +0500

    add(MyApp): this has been wrapped with init state staful widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,21 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    serviceLocator.get<AuthBloc>().add(AuthIsUserLoggedIn());
+  }
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
