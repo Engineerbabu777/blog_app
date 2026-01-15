4b37831e046e8b78e92db76c45e1fc925f378a47
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-15 16:32:46 +0500

    add(BlogEditor): this empty widget has been added

---
+++ lib/features/blog/presentation/widgets/blog_editor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BlogEditor extends StatelessWidget {
+  const BlogEditor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
