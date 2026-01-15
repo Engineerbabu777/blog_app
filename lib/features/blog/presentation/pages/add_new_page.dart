ed822c698cc5e0ed6364d12d2ae66d300dc14ef1
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-15 15:58:38 +0500

    add(AddNewBlog): emty widget has been added

---
+++ lib/features/blog/presentation/pages/add_new_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewBlog extends StatefulWidget {
+  const AddNewBlog({super.key});
+
+  @override
+  State<AddNewBlog> createState() => _AddNewBlogState();
+}
+
+class _AddNewBlogState extends State<AddNewBlog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
