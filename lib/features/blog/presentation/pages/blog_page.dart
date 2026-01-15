c31a1084bd50508813389fecdc406d0d90236cfa
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-15 16:01:12 +0500

    add(route): static route method has been added for blog pages

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -2,15 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BlogPage extends StatelessWidget {
+  static route() => MaterialPageRoute(builder: (_) => BlogPage());
   const BlogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Blog Page'), 
-      actions: [
-        IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.add_circled)),
-      ],),
+      appBar: AppBar(
+        title: Text('Blog Page'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.add_circled)),
+        ],
+      ),
     );
   }
 }
