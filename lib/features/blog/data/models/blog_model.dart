c0ac6aef8b0cc6145bb3a7ce22e480d435337a83
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 15:09:23 +0500

    add(BlogModel): this has been added

--- lib/features/blog/data/models/blog_model.dart
+++ lib/features/blog/data/models/blog_model.dart
@@ -1,3 +1,13 @@
+import 'package:blog_app/features/blog/domain/entities/blog_entity.dart';
 
-
-class BlogModel {}
+class BlogModel extends BlogEntity {
+  BlogModel({
+    required super.id,
+    required super.posterId,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.topics,
+    required super.updatedAt,
+  });
+}
