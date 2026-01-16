fbe7972b73dfa10eb4b2b07d3ef7bf28ed724083
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 16:40:15 +0500

    add(uploadBlog): this has been added new interface method in blog remote data source interface

--- lib/features/blog/data/datasources/blog_remote_datasource.dart
+++ lib/features/blog/data/datasources/blog_remote_datasource.dart
@@ -1,4 +1,5 @@
+import 'package:blog_app/features/blog/data/models/blog_model.dart';
 
-
-
-abstract interface class BlogRemoteDatasource {}
+abstract interface class BlogRemoteDatasource {
+  Future<BlogModel> uploadBlog(BlogModel blog);
+}
