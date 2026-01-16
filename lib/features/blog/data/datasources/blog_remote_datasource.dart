cb81aeeffda56734ea61fb5c997752c6761b4457
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 17:11:03 +0500

    add(uploadBlogImage): this is added in datasource impl

--- lib/features/blog/data/datasources/blog_remote_datasource.dart
+++ lib/features/blog/data/datasources/blog_remote_datasource.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:blog_app/core/error/exception.dart';
 import 'package:blog_app/features/blog/data/models/blog_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class BlogRemoteDatasource {
   Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  });
 }
 
 class BlogRemoteDatasourceImpl implements BlogRemoteDatasource {
@@ -18,6 +24,21 @@ class BlogRemoteDatasourceImpl implements BlogRemoteDatasource {
       return BlogModel.fromJson(blogData.first);
     } catch (e) {
       print('Error during blog creation: $e'); // Debugging line
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  }) async {
+    try {
+      await supabaseClient.storage.from("blog_images").upload(blog.id, image);
+
+      return supabaseClient.storage.from('blog_images').getPublicUrl(blog.id);
+    } catch (e) {
+      print('Error during blog image uploading: $e'); // Debugging line
       throw ServerException(e.toString());
     }
   }
