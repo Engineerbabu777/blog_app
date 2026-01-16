5a2ca75d54b8e68fb928b19351a6063501905b0e
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 17:36:27 +0500

    add(uploadBlog): this is done

--- lib/features/blog/data/repositories/blog_repository_impl.dart
+++ lib/features/blog/data/repositories/blog_repository_impl.dart
@@ -1,11 +1,21 @@
 import 'dart:io';
 
+import 'package:blog_app/core/error/exception.dart';
 import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_datasource.dart';
+import 'package:blog_app/features/blog/data/models/blog_model.dart';
 import 'package:blog_app/features/blog/domain/entities/blog_entity.dart';
 import 'package:blog_app/features/blog/domain/repository/blog_repository.dart';
-import 'package:fpdart/src/either.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:uuid/uuid.dart';
 
 class BlogRepositoryImpl implements BlogRepository {
+  final BlogRemoteDatasourceImpl _blogRemoteDatasourceImpl;
+
+  BlogRepositoryImpl({
+    required BlogRemoteDatasourceImpl blogRemoteDatasourceImpl,
+  }) : _blogRemoteDatasourceImpl = blogRemoteDatasourceImpl;
+
   @override
   Future<Either<Failure, BlogEntity>> uploadBlog({
     required File image,
@@ -13,8 +23,38 @@ class BlogRepositoryImpl implements BlogRepository {
     required String content,
     required String posterId,
     required List<String> topics,
-  }) {
-    // TODO: implement uploadBlog
-    throw UnimplementedError();
+  }) async {
+    try {
+      BlogModel blogModel = BlogModel(
+        id: const Uuid().v1(),
+        posterId: posterId,
+        title: title,
+        content: content,
+        imageUrl: '',
+        topics: topics,
+        updatedAt: DateTime.now(),
+      );
+
+      final blogImage = await _blogRemoteDatasourceImpl.uploadBlogImage(
+        image: image,
+        blog: blogModel,
+      );
+
+      blogModel = blogModel.copyWith(imageUrl: blogImage);
+
+      final uploadedBlog = await _blogRemoteDatasourceImpl.uploadBlog(
+        blogModel,
+      );
+
+      return Right(uploadedBlog);
+    } on ServerException catch (e) {
+      return Left(
+        Failure(
+          e.message.isNotEmpty
+              ? e.message.toString()
+              : "An unexpected error occur in upload blog",
+        ),
+      );
+    }
   }
 }
