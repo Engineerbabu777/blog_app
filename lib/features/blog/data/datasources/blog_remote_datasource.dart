b1d99f7efeb5f1b650c5cc08f9449f5b9ead8d1c
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 16:41:24 +0500

    add(uploadBlog): this has been added method in impl

--- lib/features/blog/data/datasources/blog_remote_datasource.dart
+++ lib/features/blog/data/datasources/blog_remote_datasource.dart
@@ -1,5 +1,21 @@
 import 'package:blog_app/features/blog/data/models/blog_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class BlogRemoteDatasource {
   Future<BlogModel> uploadBlog(BlogModel blog);
+}
+
+class BlogRemoteDatasourceImpl implements BlogRemoteDatasource {
+
+  final SupabaseClient supabaseClient;
+
+  @override
+  Future<BlogModel> uploadBlog(BlogModel blog) {
+   try {
+
+   } catch(e) {
+    
+   }
+  }
+  
 }
