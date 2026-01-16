87a487449451c6511aed4bbeecfb1e4c3c92fb80
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-16 15:03:29 +0500

    add(BlogEntity): entities and contructore has been added

--- lib/features/blog/domain/entities/blog_entity.dart
+++ lib/features/blog/domain/entities/blog_entity.dart
@@ -1,2 +1,19 @@
+class BlogEntity {
+  final String id;
+  final String posterId;
+  final String title;
+  final String content;
+  final String imageUrl;
+  final List<String> topics;
+  final DateTime updatedAt;
 
-class BlogEntity{}
+  BlogEntity({
+    required this.id,
+    required this.posterId,
+    required this.title,
+    required this.content,
+    required this.imageUrl,
+    required this.topics,
+    required this.updatedAt,
+  });
+}
