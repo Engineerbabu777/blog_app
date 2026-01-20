bcdb25f9608c3057098598d294363824d92ca04b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-20 10:12:39 +0500

    add(_initBlog): updated

--- lib/init_dep.dart
+++ lib/init_dep.dart
@@ -10,6 +10,7 @@ import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_datasource.dart';
 import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:blog_app/features/blog/domain/repository/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -74,7 +75,13 @@ void _initBlog() {
     ..registerFactory<UploadBlogUseCase>(
       () => UploadBlogUseCase(blogRepository: serviceLocator()),
     )
+    ..registerFactory<GetAllBlogsUseCase>(
+      () => GetAllBlogsUseCase(blogRepository: serviceLocator()),
+    )
     ..registerLazySingleton(
-      () => BlogBloc(uploadBlogUseCase: serviceLocator()),
+      () => BlogBloc(
+        uploadBlogUseCase: serviceLocator(),
+        getAllBlogs: serviceLocator(),
+      ),
     );
 }
