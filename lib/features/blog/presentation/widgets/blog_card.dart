95b689b8e36ac88d4b3d92c26fe8bc1fe319f605
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-20 10:40:31 +0500

    add(BlogCard): empty widget has been added

---
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BlogCard extends StatelessWidget {
+  const BlogCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
