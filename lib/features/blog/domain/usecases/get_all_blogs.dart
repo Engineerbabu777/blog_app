d64c501803c3a0738c7f941ed25b4cf9cf64cc92
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-20 09:55:31 +0500

    add(GetAllBlogs): done use case has ben added

--- lib/features/blog/domain/usecases/get_all_blogs.dart
+++ lib/features/blog/domain/usecases/get_all_blogs.dart
@@ -1,3 +1,19 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/blog/domain/entities/blog_entity.dart';
+import 'package:blog_app/features/blog/domain/repository/blog_repository.dart';
+import 'package:fpdart/src/either.dart';
 
+class GetAllBlogs implements UseCase<List<BlogEntity>, NoParams> {
+  final BlogRepository _blogRepository;
 
-class GetAllBlogs {}
+  GetAllBlogs({required BlogRepository blogRepository})
+    : _blogRepository = blogRepository;
+
+  @override
+  Future<Either<Failure, List<BlogEntity>>> call(NoParams params) async {
+    return await _blogRepository.getAllBlogs();
+  }
+}
+
+class NoParams {}
