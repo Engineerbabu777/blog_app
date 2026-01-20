6ecc9fe3569ed4b559e44d3faaaa12e71e97456b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-20 10:58:19 +0500

    add(calculateReadingTime): this is now ready

---
+++ lib/core/utils/calculate_reading_time.dart
@@ -0,0 +1,8 @@
+int calculateReadingTime(String content) {
+  final wordCount = content.split(RegExp(r'\s+')).length;
+   // speed = d/t;
+
+  final readingTime = 225 / wordCount;
+
+  return readingTime.ceil();
+}
